a175c804350861fa9fc25b4113314b9e818ca41a
Richard Imaoka <richard.e.imaoka@gmail.com>
2022-01-15 21:40:48 +0900

    init

---
+++ answers/index6.ts
@@ -0,0 +1,22 @@
+import { ApolloServer, gql } from "apollo-server";
+
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
+
+const resolvers = {
+  Query: {
+    hello(parent: any, args: any, context: any, info: any): string { 
+      return "hello universe!";
+    }
+  }
+}
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+// The `listen` method launches a web server.
+server.listen().then(({ url }) => {
+  console.log(`🚀  Server ready at ${url}`);
+});
---
+++ answers/index7.ts
@@ -0,0 +1,39 @@
+import { ApolloServer, gql } from "apollo-server";
+
+const typeDefs = gql`
+  type Query {
+    hello: String
+    booleanValue: Boolean
+    intValue: Int
+    floatValue: Float
+
+    arrayOfInts: [Int]
+  }
+`;
+
+const resolvers = {
+  Query: {
+    hello(parent: any, args: any, context: any, info: any): string { 
+      return "hello universe!";
+    },
+    booleanValue(parent: any, args: any, context: any, info: any): boolean { 
+      return true;
+    },
+    intValue(parent: any, args: any, context: any, info: any): number { 
+      return 3;
+    },
+    floatValue(parent: any, args: any, context: any, info: any): number { 
+      return 30.5;
+    },
+    arrayOfInts(parent: any, args: any, context: any, info: any): number[] { 
+      return [1,2,3,4,5];
+    }
+  }
+}
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+// The `listen` method launches a web server.
+server.listen().then(({ url }) => {
+  console.log(`🚀  Server ready at ${url}`);
+});
