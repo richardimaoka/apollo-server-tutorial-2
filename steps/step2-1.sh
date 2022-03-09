a32896d931c0061897428a62d9b486b796b87375
Richard Imaoka <richard.e.imaoka@gmail.com>
2022-03-09 23:32:21 +0900

    step 2

---
+++ steps/step2-1.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# ## Apollo Server立ち上げ
+
+# :large_orange_diamond: Action: 以下のコマンドを実行してください
+
+# ```terminal: メイン (ターミナル)
+cp answers/index1.ts index.ts
+# ```
---
+++ steps/step2-2.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+# :large_orange_diamond: Action: **別のターミナルを立ち上げ**、以下のコマンドを実行してください
+
+# ```terminal: Apollo Server1 (ターミナル)
+npm run start
+# ```
+
+# :white_check_mark: Result: 以下のように表示されます。
+
+# ```terminal: Apollo Server (ターミナル)
+# 🚀  Server ready at http://localhost:4000/
+# ```
+
+# :large_orange_diamond: Action: ブラウザで http://localhost:4000/ を開いてください
+
+# :white_check_mark: Result: 以下のような画面が表示されます。
+
+# ![image.png](https://qiita-image-store.s3.ap-northeast-1.amazonaws.com/0/75738/58dd6755-b37b-9f64-3047-a1a2e8e7b0b9.png)
+
+# :large_orange_diamond: Action: Query your serverボタンを押してください
+
+# :white_check_mark: Result: 以下のような画面が表示されます。
+
+# ![2022-01-25_00h35_02.png](https://qiita-image-store.s3.ap-northeast-1.amazonaws.com/0/75738/0d0ce96f-db92-760c-801e-3cff25131d5a.png)
+
+# :large_orange_diamond: Action: 以下のクエリを入力して"Run"ボタンを押してください
+
+# ```
+# {
+#   hello
+# }
+# ```
+
+# :white_check_mark: Result: Responseにnullが返ってきます
+
+# ![2022-01-28_08h59_42.png](https://qiita-image-store.s3.ap-northeast-1.amazonaws.com/0/75738/2aa1004a-2ca0-9880-1234-cd326e39260b.png)
+
+# nullが返ってくるのはResolverを実装していないからです。ここから先はでResolverを実装しましょう。
